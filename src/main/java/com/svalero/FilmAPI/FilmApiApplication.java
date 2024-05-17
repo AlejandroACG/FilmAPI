c3a0d23d054222fa49cc67420921c37134b23faf
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2024-05-17 11:24:54 +0200

    Improved code spacing

--- src/main/java/com/svalero/FilmAPI/FilmApiApplication.java
+++ src/main/java/com/svalero/FilmAPI/FilmApiApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FilmApiApplication.class, args);
 	}
-
 }
