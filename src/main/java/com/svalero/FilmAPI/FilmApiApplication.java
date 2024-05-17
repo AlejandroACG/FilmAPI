536e8f45aaa48d0d6cca47ec6458a26cd1f0abc9
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2024-05-17 10:19:57 +0200

    Used Spring Initializr

---
+++ src/main/java/com/svalero/FilmAPI/FilmApiApplication.java
@@ -0,0 +1,12 @@
+package com.svalero.FilmAPI;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/FilmAPI/FilmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.FilmAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
