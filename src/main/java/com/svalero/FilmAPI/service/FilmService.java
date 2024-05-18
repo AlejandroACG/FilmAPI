586ae0a5fabb9ab37d911c558c52777043d90bf4
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2024-05-18 12:47:38 +0200

    Add: Bodies to postman collection. Fix: FilmService

--- src/main/java/com/svalero/FilmAPI/service/FilmService.java
+++ src/main/java/com/svalero/FilmAPI/service/FilmService.java
@@ -2,7 +2,9 @@ package com.svalero.FilmAPI.service;
 import com.svalero.FilmAPI.domain.Film;
 import com.svalero.FilmAPI.dto.FilmInDto;
 import com.svalero.FilmAPI.exception.EntityNotFoundException;
+import com.svalero.FilmAPI.repository.DirectorRepository;
 import com.svalero.FilmAPI.repository.FilmRepository;
+import com.svalero.FilmAPI.repository.ProductionCompanyRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,10 @@ public class FilmService {
     @Autowired
     private FilmRepository filmRepository;
     @Autowired
+    private DirectorRepository directorRepository;
+    @Autowired
+    private ProductionCompanyRepository productionCompanyRepository;
+    @Autowired
     private ModelMapper modelMapper;
     private final String entityName = "Film";
 
@@ -22,7 +28,20 @@ public class FilmService {
     }
 
     public Film addFilm(FilmInDto filmInDto) {
-        return filmRepository.save(modelMapper.map(filmInDto, Film.class));
+        long productionCompanyId = filmInDto.getProductionCompanyId();
+        long directorId = filmInDto.getDirectorId();
+        if (productionCompanyRepository.existsById(productionCompanyId)) {
+            if (directorRepository.existsById(directorId)) {
+                Film film = modelMapper.map(filmInDto, Film.class);
+                film.setProductionCompany(productionCompanyRepository.findById(productionCompanyId).orElse(null));
+                film.setDirector(directorRepository.findById(directorId).orElse(null));
+                return modelMapper.map(filmRepository.save(film), Film.class);
+            } else {
+                throw new EntityNotFoundException("Director", filmInDto.getDirectorId());
+            }
+        } else {
+            throw new EntityNotFoundException("Production Company", filmInDto.getProductionCompanyId());
+        }
     }
 
     public Film getFilmById(long id) {
